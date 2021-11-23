99648ee500aa3409e513b8405e3790f5182f4b65
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-11-23 17:39:26 -0600

    This commit adds a small ammount of code into the user routes on how they will be structured.

--- controller/api/userRoutes.js
+++ controller/api/userRoutes.js
@@ -1,7 +1,33 @@
 const router = require('express').Router();
+const { Users } = require('../../models');
 
 //login routes
 // /, /login, and /logout
+
+router.post('/', async (req,res) => {
+    try{
+
+    }
+    catch{
+
+    }
+})
+router.post('/login', async (req,res) =>{
+    try{
+
+    }
+    catch{
+        
+    }
+})
+router.post('logout', async (req,res) =>{
+    try{
+
+    }
+    catch{
+        
+    }
+})
 
 
 
