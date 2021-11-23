7e6f77b9208dca8c2b5a5c622ae18eaac55440f1
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-11-23 17:44:33 -0600

    This commit adds the logic for saving the session with the slash route in userroutes.

--- controller/api/userRoutes.js
+++ controller/api/userRoutes.js
@@ -6,10 +6,18 @@ const { Users } = require('../../models');
 
 router.post('/', async (req,res) => {
     try{
+        const usersData = await Users.create(req.body);
 
+        //creates and sets the seession variables when this route is hit
+        //if route fails it will throw an error
+        req.session.save(() => {
+            req.session.user_id = usersData.isSoftDeleted;
+            req.session.logged_in = true;
+            res.status(200).json(usersData)
+        })
     }
-    catch{
-
+    catch(err){
+        res.status(400).json(err)
     }
 })
 router.post('/login', async (req,res) =>{
