2612873fa2c81486ff6397a0c642c08602ad1740
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-11-24 00:50:41 -0600

    This commit adds the routes for the dashboard.

--- controller/api/dashboard.js
+++ controller/api/dashboard.js
@@ -1,9 +1,53 @@
 const router = require('express').Router();
+const { Posts } = require('../../models');
+const authentication = require('../../utils/authenticator');
 
 //post put and delete routes for the users own posts.
 
-router.get();
-router.post();
-router.delete();
+router.post('/',authentication, async (req, res) => {
+    try{
+        const newPost = await Posts.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        })
+        res.status(200).json(newPost)
+    }
+    catch(err){
+        res.status(400).json(err);
+    }
+});
+
+router.put('/update/:id',authentication, async (req, res) => {
+    try{
+        const updatePost = await Posts.update( 
+            { 
+                post_title: req.body.title, 
+                content: req.body.content 
+            },
+            {
+            where:{
+                id: req.params.id
+            }
+        });
+        res.status(200).json(updatePost);
+    }
+    catch(err){
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/delete/:id',authentication, async (req, res) => {
+    try{
+        const deletePost = await Posts.destroy({
+            where: {
+                id: req.params.id
+            },
+        });
+        res.status(200).json(deletePost);
+    }
+    catch(err){
+        res.status(400).json(err);
+    }
+});
 
 module.exports = router;
--- public/js/dashbaord.js
+++ public/js/dashbaord.js
@@ -3,6 +3,8 @@ var deletebtn = document.querySelector('');
 var updatebtn = document.querySelector('');
 
 
+//send the current id in the update route
+
 updatebtn.addEventListener('submit', updatePost);
 deletebtn.addEventListener('submit', deletePost)
 createbtn.addEventListener('submit', createPost)
