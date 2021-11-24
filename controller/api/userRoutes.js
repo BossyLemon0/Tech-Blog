c365be6c07d31c4fcea004e2d7bc021b9e2f5a31
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-11-23 18:12:24 -0600

    This commit adds the logic for logging in with a preexisting user.

--- controller/api/userRoutes.js
+++ controller/api/userRoutes.js
@@ -6,14 +6,14 @@ const { Users } = require('../../models');
 
 router.post('/', async (req,res) => {
     try{
-        const usersData = await Users.create(req.body);
+        const newUsersData = await Users.create(req.body);
 
         //creates and sets the seession variables when this route is hit
         //if route fails it will throw an error
         req.session.save(() => {
-            req.session.user_id = usersData.isSoftDeleted;
+            req.session.user_id = newUsersData.id;
             req.session.logged_in = true;
-            res.status(200).json(usersData)
+            res.status(200).json(newUsersData)
         })
     }
     catch(err){
@@ -22,10 +22,29 @@ router.post('/', async (req,res) => {
 })
 router.post('/login', async (req,res) =>{
     try{
+        //in the front end we have to pass username and password
+        const userInfo = await Users.findOne({ where: {username: req.body.username}});
+        if(!userLogin){
+            res.status(400).json({ message: 'Incorrect Username, please try again' });
+          return;
+        }
+        //we have to hash the password somewhere along the way.
+        const userPassword = await userInfo.checkPassword(req.body.userPassword)
+        if(!userPassword){
+            res.status(400).json({ message: 'Incorrect Password, please try again' });
+          return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userInfo.id;
+            req.session.logged_in = true;
+            
+            res.json({ user: userInfo, message: 'Logged in' });
+          });
 
     }
-    catch{
-        
+    catch(err){
+        res.status(400).json(err)
     }
 })
 router.post('logout', async (req,res) =>{
--- models/Users.js
+++ models/Users.js
@@ -15,17 +15,9 @@ class Users extends Model {
             allowNull: false,
             primaryKey: true
           },
-          name: {
+          username: {
             type: DataTypes.STRING,
             allowNull: false,
-          },
-          email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-              isEmail: true,
-            },
           },
           password: {
             type: DataTypes.STRING,
